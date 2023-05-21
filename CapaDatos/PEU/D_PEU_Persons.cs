31efa1d2fa1bdb1b41a657caa34d1705ca5149f2
edgar <epacotaipe@gmail.com>
2023-05-21 15:29:31 -0500

    cambiando la logica

---
+++ CapaDatos/PEU/D_PEU_Persons.cs
@@ -0,0 +1,55 @@
+﻿using CapaDatos.DB;
+using CapaEntidades.PEU;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Configuration.Provider;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos.PEU
+{
+    public class D_PEU_Listar
+    {
+        private SqlConnection cn;
+        private Conexion MiConexi = new Conexion();
+        private E_PEU_Persona persona = new E_PEU_Persona();
+        SqlCommand cmd = new SqlCommand();
+
+        public List<E_PEU_Persona> D_listEmpleados()
+        {
+            List<E_PEU_Persona> E_list = new List<E_PEU_Persona>();
+            try
+            {
+                cn = new SqlConnection(ConfigurationManager.ConnectionStrings["desbaco"].ConnectionString);
+                cmd = new SqlCommand("listame", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cn.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    E_PEU_Persona e_PEU_Persona = new E_PEU_Persona();
+                    e_PEU_Persona.id = rdr["PersonaID"].ToString();
+                    e_PEU_Persona.Nombre = rdr["LastName"].ToString();
+                    E_list.Add(e_PEU_Persona);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Errror al iniciar el sistema " + e);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return E_list;
+        }
+
+
+
+
+
+
+    }
+
+}
---
+++ test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+namespace test
+{
+    public class Program
+    {
+  
+        static void Main(string[] args)
+        {
+            string DB = "empresa";
+            string Servidor = "DESKTOP-NFF8OB1\\SQLEXPRESS";
+            string Usuario = "";
+            string Clave = "";
+
+            try
+            {
+                string connectionString = $"Data Source={Servidor};Initial Catalog={DB}; Integrated Security = true;";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                }
+                Console.WriteLine("Exito");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al conectar a la base de datos: " + ex.Message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
