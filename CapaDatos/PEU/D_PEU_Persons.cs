34add17daa91468c5d513fdb3a4011207cc34f00
edgar <epacotaipe@gmail.com>
2023-05-22 00:32:04 -0500

    configuracion desbaco

--- CapaDatos/PEU/D_PEU_Persons.cs
+++ CapaDatos/PEU/D_PEU_Persons.cs
@@ -12,21 +12,21 @@ namespace CapaDatos.PEU
     public class D_PEU_Persons
     {
        
-     public List<E_PEU_Persons> D_ListPerson()
-        {
+         public List<E_PEU_Persons> D_ListPerson()
+         {
             List<E_PEU_Persons> E_list = new List<E_PEU_Persons>();
             try
             {
                 SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["desbaco"].ConnectionString);
-                SqlCommand cmd = new SqlCommand("listame", cn);
+                SqlCommand cmd = new SqlCommand("SP_APP_TESTEO_DETALLE", cn);
                 cmd.CommandType = CommandType.StoredProcedure;
                 cn.Open();
                 SqlDataReader rdr = cmd.ExecuteReader();
                 while (rdr.Read())
                 {
                     E_PEU_Persons e_PEU_Persons = new E_PEU_Persons();
-                    e_PEU_Persons.Nombre = rdr["LastName"].ToString();
-                    e_PEU_Persons.Apellido = rdr["FirstName"].ToString();
+                    e_PEU_Persons.Nombre = rdr["des_nombre"].ToString();
+                    e_PEU_Persons.Apellido = rdr["des_ape_pat"].ToString();
                     E_list.Add(e_PEU_Persons);
 
                 }
@@ -39,11 +39,12 @@ namespace CapaDatos.PEU
             return E_list;
         }
 
-
-
-
-
-
     }
+
+    //CREATE PROCEDURE SP_APP_TESTEO_DETALLE
+    //    AS
+    //    BEGIN
+    //    SELECT*, des_ape_pat FROM[dbo].[crm_contacto]
+    //        END
 
 }
