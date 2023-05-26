e66e4ef99318258fdf3bf13ab31e7478a1d16414
edgar <epacotaipe@gmail.com>
2023-05-26 00:56:17 -0500

    modo using

--- CapaDatos/PEU/D_PEU_Persons.cs
+++ CapaDatos/PEU/D_PEU_Persons.cs
@@ -11,21 +11,19 @@ namespace CapaDatos.PEU
     public class D_PEU_Persons
     {
         private List<E_PEU_Persons> E_list = new List<E_PEU_Persons>();
-        private SqlConnection connection;
-        private SqlDataReader reader;
-        private Conexion con = new Conexion();
-        public async  Task <List<E_PEU_Persons>> D_ListPerson()
+        private readonly Conexion con = new Conexion();
+        public async  Task <List<E_PEU_Persons>> D_ListPerson4()
         {       
             try
             {
                  return await Task.Run(() => {
 
-                     using (connection = new SqlConnection(con.connection()))
+                     using (SqlConnection connection = new SqlConnection(con.connection()))
                      {
                          SqlCommand cmd = new SqlCommand("SP_APP_TESTEO_DETALLE", connection);
                          cmd.CommandType = CommandType.StoredProcedure;
                          connection.Open();
-                         reader = cmd.ExecuteReader();
+                         SqlDataReader  reader = cmd.ExecuteReader();
                          while (reader.Read())
                          {
                              E_PEU_Persons e_PEU_Persons = new E_PEU_Persons();
@@ -42,19 +40,49 @@ namespace CapaDatos.PEU
             {
                 throw new Exception("Errror al iniciar el sistema " + e);
             }
-            finally
-            {
-              
-                if (reader != null)  { reader.Close(); }
-                if (connection != null)  {  connection.Close(); }
-            }
-
-           
-
           
         }
 
-    }
+
+        public async Task<List<E_PEU_Persons>> D_ListPerson()
+        {
+
+            using (SqlConnection connection = new SqlConnection(con.connection()))
+            {
+                try
+                {
+
+                    connection.Open();
+                  
+                    using (SqlCommand command = new SqlCommand("SP_APP_TESTEO_DETALLE", connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            command.CommandType = CommandType.StoredProcedure;
+                            return await Task.Run(() => {
+                                    while (reader.Read())
+                                    {
+                                        E_PEU_Persons e_PEU_Persons = new E_PEU_Persons();
+                                        e_PEU_Persons.Nombre = reader["des_nombre"].ToString();
+                                        e_PEU_Persons.Apellido = reader["des_ape_pat"].ToString();
+                                        E_list.Add(e_PEU_Persons);
+                                    }
+                                return E_list;
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Errror al iniciar el sistema " + ex);
+                }
+            }
+
+
+        }
+
+
+        }
 
     //CREATE PROCEDURE SP_APP_TESTEO_DETALLE
     //    AS
